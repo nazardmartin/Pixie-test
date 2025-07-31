108c8bc3444b57744d54ec4071f3bde414fb1857
nazardmartin <mburunazard@gmail.com>
2025-07-31 15:06:28 +0300

    Update MainActivity.java

--- app/src/main/java/com/pixie/app/MainActivity.java
+++ app/src/main/java/com/pixie/app/MainActivity.java
@@ -1,1 +1,23 @@
+package com.pixie.app;
 
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.view.Gravity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setGravity(Gravity.CENTER);
+        TextView textView = new TextView(this);
+        textView.setText("Welcome to Pixie!");
+        textView.setTextSize(20);
+
+        layout.addView(textView);
+        setContentView(layout);
+    }
+}
